055a2228f326daf40d418fc7134b1aceffafa1f5
Ulez <1104128773@qq.com>
2018-03-04 23:21:10 +0800

    s搜索；

--- booktest/admin.py
+++ booktest/admin.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 from .models import *
 class BookInfoAdmin(admin.ModelAdmin):
     list_display = ['id','btitle','bpub_date']#定义显示的内容；
+    search_fields = ['btitle']
 admin.site.register(BookInfo,BookInfoAdmin) #规定每一行的显示内容；
 admin.site.register(HeroInfo)
