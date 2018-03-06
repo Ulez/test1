69194191c9d2e9c9840d504ce2f0858d6cf8bdb7
Ulez <1104128773@qq.com>
2018-03-07 00:25:19 +0800

    查询与传递；

--- booktest/views.py
+++ booktest/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import *
 from django.template import RequestContext,loader
+from .models import *
 # Create your views here.
 def index(request):
-    context={'title':'hello5646456'}
+    context={'list':BookInfo.objects.all()}
     return render(request,'booktest/index.html',context)
