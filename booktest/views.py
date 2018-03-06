befbc4b6ca9cc7bd6d351bcfe0088fc07c861b98
Ulez <1104128773@qq.com>
2018-03-07 00:17:22 +0800

    向视图传值；

--- booktest/views.py
+++ booktest/views.py
@@ -3,5 +3,5 @@ from django.http import *
 from django.template import RequestContext,loader
 # Create your views here.
 def index(request):
-    temp=loader.get_template('booktest/index.html')
-    return HttpResponse(temp.render())
+    context={'title':'hello5646456'}
+    return render(request,'booktest/index.html',context)
