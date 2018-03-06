3d7c0760a4cf2ea6a75da5f3c839f2a45166be68
Ulez <1104128773@qq.com>
2018-03-07 00:09:18 +0800

    视图使用模板显示；

--- booktest/views.py
+++ booktest/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import *
+from django.template import RequestContext,loader
 # Create your views here.
 def index(request):
-    return HttpResponse('hello world')
+    temp=loader.get_template('booktest/index.html')
+    return HttpResponse(temp.render())
